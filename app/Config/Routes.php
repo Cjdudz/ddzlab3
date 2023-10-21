074c3350d27b812fb6464503374cc2459d308745
ddz <130657061+Cjdudz@users.noreply.github.com>
2023-10-21 13:59:05 +0800

    asdadad

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/admin', 'Home::admin', ['filter' => 'authGuard']);
+$routes->post('/signin', 'UserController::LoginAuth');
+$routes->match(['get', 'post'], '/signup', 'UserController::register', ['filter' => 'authGuard']);
+$routes->get('/logout', 'UserController::logout');
+$routes->get('/login', 'UserController::login');
